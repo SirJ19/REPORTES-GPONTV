151f2a1a3d4f9a16ad1c434fa6e98cfb0658afcb
JBEDON <149858532+SirJ19@users.noreply.github.com>
2026-09-13 03:39:23 -0500

    Add files via upload

---
+++ dtvsol-sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil((async () => {
+    const windows = await self.clients.matchAll({ type: 'window', includeUncontrolled: true });
+    for (const client of windows) {
+      if ('focus' in client) {
+        await client.focus();
+        return;
+      }
+    }
+    if (self.clients.openWindow) await self.clients.openWindow('./');
+  })());
+});
